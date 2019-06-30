586c29d279253634bf428797f6c136898430803f
Conor Brady <conorpbrady@gmail.com>
2019-06-30 18:02:43 -0400

    complete diceSet

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -6,6 +6,18 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_accessor :values
+
+  def roll(n)
+    @values = []
+    n.times { @values << Random.new.rand(1..6)}
+  end
+  def values
+    @values
+  end
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
